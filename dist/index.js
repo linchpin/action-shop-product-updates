e3dde6777e4ce9e796a6a6c1ef8c915e8680609e
Aaron Ware <aware@linchpinagency.com>
2019-12-18 17:18:26 -0500

    Updating product meta not actual variation meta

--- dist/index.js
+++ dist/index.js
@@ -449,50 +449,36 @@ const api                = new WooCommerceRestApi( {
 } );
 
 let woocommerce = function () {
-	api.get( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) + "/variations" )
+
+	core.debug( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) + "/variations" );
+
+	// get out release date in the proper format
+	let today = new Date();
+	let dd    = String( today.getDate() ).padStart( 2, '0' );
+	let mm    = String( today.getMonth() + 1 ).padStart( 2, '0' );
+	let yyyy  = today.getFullYear();
+
+	today = yyyy + '/' + mm + '/' + dd;
+
+	let download_data = {
+		meta_data: [
+			{
+				'key' : '_api_last_updated',
+				'value' : today
+			},
+			{
+				'key' : '_api_new_version',
+				'value' : core.getInput('woo_product_version' )
+			}
+		]
+	};
+
+	api.put( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ), download_data )
 		.then( ( response ) => {
-
-			// get our variations.
-			let variations = response.data;
-
-			// get out release date in the proper format
-			let today = new Date();
-			let dd    = String( today.getDate() ).padStart( 2, '0' );
-			let mm    = String( today.getMonth() + 1 ).padStart( 2, '0' );
-			let yyyy  = today.getFullYear();
-
-			today = yyyy + '/' + mm + '/' + dd;
-
-			// loop through our software and update accordingly
-			variations.forEach( element => {
-
-				let download_data = {
-					meta_data: [
-						{
-							'key' : '_api_last_updated',
-							'value' : today
-						},
-						{
-							'key' : '_api_new_version',
-							'value' : core.getInput('woo_product_version' )
-						}
-					]
-				};
-
-				core.debug(`Updating Product ID: ${element.id}`);
-
-				api.put( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) + "/variations/" + parseInt( element.id ), download_data )
-					.then( ( response ) => {
-						core.debug( response.data );
-					} )
-					.catch( ( error ) => {
-						core.setFailed( `Product Meta failed with error ${error}` );
-					} );
-			} );
-
+			core.debug( 'Product Meta updated:' + core.getInput( 'woo_product_id' ) );
 		} )
 		.catch( ( error ) => {
-			core.setFailed( `Action failed with error ${error}` );
+			core.setFailed( `Product Meta failed with error ${error}` );
 		} );
 };
 
--- woocommerce.js
+++ woocommerce.js
@@ -10,48 +10,35 @@ const api                = new WooCommerceRestApi( {
 
 let woocommerce = function () {
 
+	core.debug( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) + "/variations" );
 
+	// get out release date in the proper format
+	let today = new Date();
+	let dd    = String( today.getDate() ).padStart( 2, '0' );
+	let mm    = String( today.getMonth() + 1 ).padStart( 2, '0' );
+	let yyyy  = today.getFullYear();
 
-	api.get( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) + "/variations" )
+	today = yyyy + '/' + mm + '/' + dd;
+
+	let download_data = {
+		meta_data: [
+			{
+				'key' : '_api_last_updated',
+				'value' : today
+			},
+			{
+				'key' : '_api_new_version',
+				'value' : core.getInput('woo_product_version' )
+			}
+		]
+	};
+
+	api.put( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ), download_data )
 		.then( ( response ) => {
-
-			// get our variations.
-			let variations = response.data;
-
-			// get out release date in the proper format
-			let today = new Date();
-			let dd    = String( today.getDate() ).padStart( 2, '0' );
-			let mm    = String( today.getMonth() + 1 ).padStart( 2, '0' );
-			let yyyy  = today.getFullYear();
-
-			today = yyyy + '/' + mm + '/' + dd;
-
-			let download_data = {
-				meta_data: [
-					{
-						'key' : '_api_last_updated',
-						'value' : today
-					},
-					{
-						'key' : '_api_new_version',
-						'value' : core.getInput('woo_product_version' )
-					}
-				]
-			};
-
-			core.debug(`Updating Product ID: ${element.id}`);
-
-			api.put( "products/" + parseInt( core.getInput( 'woo_product_id' ), 10 ) )
-				.then( ( response ) => {
-					core.debug( response.data );
-				} )
-				.catch( ( error ) => {
-					core.setFailed( `Product Meta failed with error ${error}` );
-				} );
-
+			core.debug( 'Product Meta updated:' + core.getInput( 'woo_product_id' ) );
 		} )
 		.catch( ( error ) => {
-			core.setFailed( `Action failed with error ${error}` );
+			core.setFailed( `Product Meta failed with error ${error}` );
 		} );
 };
 
